f6abff526f47ac6e227fcbb2c7b15e429677df5a
hoangden318 <truonghoang31082002@gmail.com>
2024-10-16 18:48:43 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world React with Hoi Dan IT
+          Hello world React with Hoang
         </p>
         <a
           className="App-link"
